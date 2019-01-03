3b5275c0010f47b4ed411da70de16d84fdea2058
skrzymo <skrzymo@gmail.com>
2019-01-03 11:50:11 +0100

    GUI mode and main menu added.

--- src/main/java/Tetris.java
+++ src/main/java/Tetris.java
@@ -5,15 +5,7 @@ public class Tetris extends JComponent {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        //TetrisGUIMain tg = new TetrisGUIMain();
-        //tg.run();
         Menu menu = new Menu();
         menu.running();
-        //menu.frame.setVisible(false);
-        //sleep(3000);
-        //menu.frame.setVisible(true);
-
-        //TetrisView view = new TetrisView();
-        //view.run();
     }
 }
